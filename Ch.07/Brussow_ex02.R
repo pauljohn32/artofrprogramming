258f7109dbca48e951a968ded71f2fcc6665d01d
jenitivecase <jbrussow@gmail.com>
2016-11-02 17:02:06 -0500

    cleaning during class

--- Ch.07/Brussow_ex02.R
+++ Ch.07/Brussow_ex02.R
@@ -1,7 +1,6 @@
 x <- c("douglas-kansas-2016-10-21", "johnson-kansas-2016-10-33", "sedgwick-kansas-2016-04-12") 
 
 cleanup <- function(x){
-  x <- tolower(x)
   x <- gsub("(?<=\\b)([a-z])", "\\U\\1", tolower(x), perl=TRUE)
   x <- gsub("Kansas", "KS", x)
   x <- strsplit(x, "-")
